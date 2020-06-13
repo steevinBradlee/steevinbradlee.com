904fe0ebf3eb5cd46f27df1127e6e64caea3b986
Stephen Bradley <stephen.jm.bradley93@gmail.com>
2020-06-13 00:55:43 -0400

    Fixing casing with components

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
-import Dump from '../components/dump';
+import Dump from '../components/Dump';
 import { Link } from 'gatsby';
 
 export default ({ data, pageContext }) => {
