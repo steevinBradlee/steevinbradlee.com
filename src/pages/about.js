87e645dd7f91228ac9ca795530142e256eb70aaf
Stephen Bradley <stephen.jm.bradley93@gmail.com>
2020-06-13 00:30:42 -0400

    Fixing casing with components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
 
-const avatar = require('../../public/github-pic.png');
-
 const AboutPage = () => {
   return (
     <Layout>
