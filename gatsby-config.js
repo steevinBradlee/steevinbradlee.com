c6b791f9eb7549bc1ad8315f73d3ddcd609b2bfe
Stephen Bradley <stephen.jm.bradley93@gmail.com>
2020-06-24 23:07:53 -0400

    Fixing issue with window being undefined when building on server in heroku

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
+    'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
