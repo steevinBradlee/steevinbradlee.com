fae38773f95e97f05521c45e658a7cd256e3dae9
Stephen Bradley <stephen.jm.bradley93@gmail.com>
2020-10-01 18:16:16 -0400

    Small change to intro page

--- src/components/intro-header/intro-hero.js
+++ src/components/intro-header/intro-hero.js
@@ -14,7 +14,7 @@ const IntroHero = () => (
         <div className='line line4'></div></span>.</span>
       </div>
     </div>
-    <div className='small'>I like front-end development and other nerdish nonsense.</div>
+    <div className='small'>Have a look around. Maybe you'll find some helpful tech tidbit.</div>
   </div>
 );
 
